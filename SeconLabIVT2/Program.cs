4436d92e2041a6609f48ffd8217030a162c7adad
dimas <dimas@GIGABYTENOTE>
2024-10-28 13:03:51 +0300

    Добавьте файлы проекта.

---
+++ SeconLabIVT2/Program.cs
@@ -0,0 +1,110 @@
+﻿using SecondLabIVT2;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Создаем экземпляр класса для доступа к методам
+        SecondLabFunctions labFunctions = new SecondLabFunctions();
+
+        Console.WriteLine("1) Решить квадратное уравнение (a, b, c)");
+        Console.WriteLine("2) Приближённое вычисление числа pi (ввод слагаемых от пользователя)");
+        Console.WriteLine("3) Вывести на экран кол-во четырехзначных чисел в ряде Фибоначчи");
+        Console.WriteLine("4) Вычислить приближенное cos(x) по формуле Тейлора (ввод x и q)");
+        Console.WriteLine("5) Найти комбинации натурального числа N");
+        Console.WriteLine("6) Правильное окончание слова (лет/год/года)");
+        Console.WriteLine("7) Разность 3 чисел (3 числа)");
+        Console.WriteLine("8) Все двузначные числа, которые делятся на 5");
+
+        int userChoice = int.Parse(Console.ReadLine());
+
+        switch (userChoice)
+        {
+            case 1:
+                Console.WriteLine("Решаем уравнение: ax^2 + bx + c = 0");
+                Console.Write("Введите a: ");
+                double letterA = double.Parse(Console.ReadLine());
+                Console.Write("Введите b: ");
+                double letterB = double.Parse(Console.ReadLine());
+                Console.Write("Введите c: ");
+                double letterC = double.Parse(Console.ReadLine());
+                labFunctions.ReshenieDiskriminant(letterA, letterB, letterC);
+                break;
+
+            case 2:
+                Console.WriteLine("Введите количество операций: ");
+                int numberOfOperations = int.Parse(Console.ReadLine());
+                labFunctions.ChisloPi(numberOfOperations);
+                break;
+
+            case 3:
+                Console.Write("Введите количество операций: ");
+                int numberOfOper = int.Parse(Console.ReadLine());
+                labFunctions.RyadFib(numberOfOper);
+                break;
+
+            case 4:
+                labFunctions.RyadTayl();
+                break;
+
+            case 5:
+                labFunctions.CombinationFind();
+                break;
+
+            case 6:
+                Console.WriteLine("Введите число от 1 до 100: ");
+                int year = int.Parse(Console.ReadLine());
+
+                if (year < 1 || year > 100)
+                {
+                    Console.WriteLine("Число должно быть в диапазоне от 1 до 100");
+                    return;
+                }
+                labFunctions.Okonchanie(year);
+                break;
+
+            case 7:
+                Console.WriteLine("Введите два числа:");
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+
+                if (num1 < 0 || num2 < 0)
+                {
+                    Console.WriteLine("Error: числа не могут быть отрицательными.");
+                }
+                else
+                {
+                    double smallerNum = Math.Min(num1, num2);
+                    double squareRoot = Math.Sqrt(smallerNum);
+                    Console.WriteLine($"Квадратный корень меньшего числа: {squareRoot}");
+                }
+                break;
+
+            case 8:
+                Console.WriteLine("Введите число от 1 до 10000:");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 1 || number > 10000)
+                {
+                    Console.WriteLine("Число должно быть от 1 до 10000.");
+                    return;
+                }
+
+                Console.WriteLine($"Нечетные делители числа {number}:");
+                for (int i = 1; i <= number; i += 2)
+                {
+                    if (number % i == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                Console.WriteLine();
+                break;
+
+            default:
+                Console.WriteLine("Неверный выбор. Пожалуйста, выберите число от 1 до 8.");
+                break;
+        }
+    }
+}
---
+++ SeconLabIVT2/second_lab_functions.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace SecondLabIVT2
+{
+    internal class SecondLabFunctions
+    {
+        // Метод решения дискриминанта
+        public void ReshenieDiskriminant(double a, double b, double c)
+        {
+            double diskriminant = b * b - 4 * a * c;
+
+            if (diskriminant > 0)
+            {
+                Console.WriteLine($"D={diskriminant}");
+                double x1 = (-b + Math.Sqrt(diskriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(diskriminant)) / (2 * a);
+                Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+            }
+            else if (diskriminant == 0)
+            {
+                Console.WriteLine($"D={diskriminant}");
+                double x1 = -b / (2 * a);
+                Console.WriteLine($"x1 = {x1}");
+            }
+            else
+            {
+                Console.WriteLine($"D={diskriminant}");
+                Console.WriteLine("Действительных корней нет. Ответ в виде комплексных чисел: ");
+                diskriminant = Math.Abs(diskriminant);
+                double x1 = -b / (2 * a);
+                Console.WriteLine($"x1={x1}-{Math.Sqrt(diskriminant)}i, x2={x1}+{Math.Sqrt(diskriminant)}i");
+            }
+        }
+
+        // Метод для расчета числа Пи
+        public void ChisloPi(int numberOfOperations)
+        {
+            if (numberOfOperations < 0)
+            {
+                Console.WriteLine("Нельзя сделать отрицательное кол-во операций");
+                return;
+            }
+
+            Console.WriteLine($"Количество слагаемых для вычисления: {numberOfOperations}");
+
+            double pi_4 = 0.0;
+            int currentSymbol = 1;
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                int denominator = 2 * i + 1;
+                double term = currentSymbol / (double)denominator;
+                pi_4 += term;
+                string sign = currentSymbol == 1 ? "+" : "-";
+                Console.WriteLine($"Шаг {i + 1}: текущее слагаемое = {sign} 1/{denominator}, сумма для pi/4 = {pi_4}");
+                currentSymbol *= -1;
+            }
+            double pi = pi_4 * 4;
+            Console.WriteLine($"Приближенное значение pi={pi}");
+        }
+
+        // Метод для ряда Фибоначчи
+        public void RyadFib(int numberOfOperations)
+        {
+            double[] f = new double[numberOfOperations];
+            int numberOf4DigitNumbers = 0;
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                if (i < 2)
+                    f[i] = 1;
+                else
+                    f[i] = f[i - 1] + f[i - 2];
+                if (f[i] >= 1000 && f[i] <= 9999)
+                    numberOf4DigitNumbers++;
+
+                Console.WriteLine($"Шаг {i} f = {f[i]}");
+            }
+            Console.WriteLine($"Всего 4-х значных элементов: {numberOf4DigitNumbers}");
+        }
+
+        // Вспомогательный метод для вычисления факториала
+        private double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        // Метод для вычисления ряда Тейлора
+        public void RyadTayl()
+        {
+            double q;
+            int x;
+            Console.WriteLine("Введите значение x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите Q: ");
+            q = double.Parse(Console.ReadLine());
+
+            double tmp = 0;
+            int step = 2;
+            do
+            {
+                if (step == 2)
+                    tmp = 1;
+                else
+                {
+                    tmp = step % 4 != 0 ?
+                        tmp - (Math.Pow(x, step) / Factorial(step)) :
+                        tmp + (Math.Pow(x, step) / Factorial(step));
+                }
+                Console.WriteLine($"Шаг = {step} вычисление = {tmp} < {q}");
+                step += 2;
+
+            } while (tmp >= q);
+        }
+
+        // Метод для поиска комбинаций кубов чисел, равных N
+        public void CombinationFind()
+        {
+            int n;
+
+            Console.WriteLine("Введите N>0");
+            n = int.Parse(Console.ReadLine());
+
+            double result = 0;
+            bool find = false;
+            int x = 0, y = 0, z = 0;
+
+            for (x = 0; x < n; x++)
+                for (y = 0; y < n; y++)
+                    for (z = 0; z < n; z++)
+                    {
+                        result = Math.Pow(x, 3) + Math.Pow(y, 3) + Math.Pow(z, 3);
+
+                        if (result == n)
+                        {
+                            Console.WriteLine(find ?
+                                "В ходе перебора нашли значение x^3 + y^3 + z^3 = N" :
+                                "В ходе перебора не нашли значения x^3 + y^3 + z^3 = N");
+                            Console.WriteLine($"{x}^3 + {y}^3 + {z}^3 = {n}");
+                            find = true;
+                        }
+                    }
+            if (!find)
+                Console.WriteLine("Ничего не найдено!");
+        }
+
+        // Метод для вывода окончания года
+        public void Okonchanie(int year)
+        {
+            int lastDigit = year % 10;
+            int lastTwoDigits = year % 100;
+
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 14)
+            {
+                Console.WriteLine($"{year} лет");
+            }
+            else if (lastDigit == 1)
+            {
+                Console.WriteLine($"{year} год");
+            }
+            else if (lastDigit >= 2 && lastDigit <= 4)
+            {
+                Console.WriteLine($"{year} года");
+            }
+            else
+            {
+                Console.WriteLine($"{year} лет");
+            }
+        }
+    }
+}
